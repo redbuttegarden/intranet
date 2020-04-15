634051914414275d35797239aafce833d2a138d0
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2020-04-15 15:23:56 -0600

    Added django-pytest to project
    Installed and established basic configuration of pytest-django for testing.
    
    Added a few basic tests to blog application.
    
    Added basic test to custom_user application.
    
    Added pytest-django version 3.9.0 to requirements.txt.
    
    Added pytest.ini file to project.
    
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- intranet/blog/tests.py
+++ intranet/blog/tests.py
@@ -1,1 +1,20 @@
-# Create your tests here.
+import pytest
+
+from .apps import BlogConfig
+from .models import BlogCategory
+
+
+@pytest.fixture()
+def blog_category_model(db):
+    blog_cat = BlogCategory.objects.create(name="Projects",
+                                           slug='projects')
+    yield blog_cat
+
+
+def test_blog_installed(settings):
+    assert BlogConfig.name in settings.INSTALLED_APPS
+
+
+def test_blog_category_string(blog_category_model):
+    blog_cat = blog_category_model
+    assert str(blog_cat) == "Projects"
--- intranet/custom_user/tests.py
+++ intranet/custom_user/tests.py
@@ -1,1 +1,5 @@
-# Create your tests here.
+from .apps import CustomUserConfig
+
+
+def test_custom_user_installed(settings):
+    assert CustomUserConfig.name in settings.INSTALLED_APPS
