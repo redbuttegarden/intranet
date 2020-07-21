2ef3ee91894581139a48f133ac47240edd4aeb66
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2020-07-21 11:34:56 -0600

    Box Disk Usage Report Blog Post
    Box report management command now creates a test blog post. Still need to add relevant data to generated blog post.
    
    Reformatted folder_sizes view split variable into constituent parts.
    
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- intranet/box/management/commands/box_report.py
+++ intranet/box/management/commands/box_report.py
@@ -1,11 +1,15 @@
+import datetime
 import glob
+import json
 import os
 import subprocess
 import time
 
-from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.core.management.base import BaseCommand
 
-from blog.models import BlogPage
+from blog.models import BlogPage, BlogCategory, BlogIndexPage
 from box.models import Folder, FolderStats
 
 
@@ -15,10 +19,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         os.chdir('/Box')
         try:
-            for line in subprocess.check_output(['du', '-d', '1'] + glob.glob('*'), encoding='utf-8').split(sep="\n"):
-                line = line.split()
-                if len(line) == 0:
+            # Run du against all top-level directories in the RBG-Shared Box Folder, 1 directory deep
+            for line in subprocess.check_output(['du', '-d', '1'] + glob.glob('*')[:2], encoding='utf-8').split(
+                    sep="\n"):
+                line = line.split(sep='\t')
+                if len(line) < 2:
+                    # The last item of the output will [''] so we avoid an IndexError by checking the list length
                     continue
+                print("Line:", line)
                 print("[*] Saving {} with size {}...".format(line[1], line[0]))
                 folder, created = Folder.objects.get_or_create(name=line[1])
                 folder.save()
@@ -28,3 +36,29 @@ class Command(BaseCommand):
             print("[!]", e)
             if 'Network is unreachable' in str(e):
                 time.sleep(10)  # Wait 10 seconds for the network
+
+        parent_page = BlogIndexPage.objects.get(slug='it-blog')
+        category = BlogCategory.objects.get(name='Inventory')
+        blogpage_content_type = ContentType.objects.get_for_model(
+            BlogPage
+        )
+        today = str(datetime.date.today())
+        title = 'Box Folder Report ' + today
+        # TODO - Get total disk usage by department and add to blog report paragraph
+        body = json.dumps([
+            {u'type': u'heading', u'value': u'Department Breakdown By Disk Usage'},
+            {u'type': u'paragraph', u'value': u'test'},
+        ])
+        new_post = BlogPage(
+            title=title,
+            draft_title=title,
+            slug=title.replace(' ', '-'),
+            show_in_menus=True,
+            content_type=blogpage_content_type,
+            author=get_user_model().objects.get(username='intranet_admin'),
+            summary='Disk usage report for top-level Box folders generated on ' + today,
+            body=body,
+        )
+        parent_page.add_child(instance=new_post)
+        new_post.categories.add(category)
+        new_post.save()
--- intranet/box/views.py
+++ intranet/box/views.py
@@ -7,7 +7,8 @@ from .tables import FolderStatTable
 
 
 def folder_sizes(request):
-    table = FolderStatTable(FolderStats.objects.all().prefetch_related('folder'))
+    stats = FolderStats.objects.all().prefetch_related('folder')
+    table = FolderStatTable(stats)
     RequestConfig(request).configure(table)
 
     return render(request, "box/box_stats.html", {
